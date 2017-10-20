dd26316a24300cc68d5e49637135256bd760cff6
John Williams <john@fortyau.com>
2017-10-20 17:37:32 -0500

    Initial commit of S3 Remote State

---
+++ main.tf
@@ -0,0 +1,22 @@
+# Author: John Williams
+# Site: https://github.com/40aujohnwilliams/s3remotestate
+# S3 Remote State Module
+
+resource "aws_s3_bucket" "remote_state_bucket" {
+  bucket = "${var.name}"
+  acl    = "private"
+  region = "${var.region}"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags {
+    Name      = "Terraform Remote State Storage"
+    terraform = true
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+# Author: John Williams
+# Site: https://github.com/40aujohnwilliams/s3remotestate
+# S3 Remote State Module Variables
+
+variable "region" {
+  description = "Region to put the bucket"
+  default     = ""
+}
+
+variable "name" {
+  description = "Name of the bucket"
+  default     = ""
+}
